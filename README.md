c02d81d80554c6087e37f63e99888c417eeb0766
冯建涛 <gastonfeng@qq.com>
2016-09-22 13:44:44 +0800

    简单的httpserver,用于获取http访问的数据

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # train_python
 python train
+
+**http server**
+简单的httpserver,用于获取http访问的数据
---
+++ httpServer.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+
+import BaseHTTPServer
+import io
+import shutil
+import urllib
+
+
+class MyRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.process(2)
+
+    def do_POST(self):
+        self.process(1)
+
+    def process(self, type):
+
+        content = ""
+        if type == 1:  # post方法，接收post参数
+            datas = self.rfile.read(int(self.headers['content-length']))
+            print datas
+            datas = urllib.unquote(datas).decode("utf-8", 'ignore')  # 指定编码方式
+            #datas = transDicts(datas)  # 将参数转换为字典
+            #if datas.has_key('data'):
+            #    content = "data:" + datas['data'] + "\r\n"
+
+        if '?' in self.path:
+            query = urllib.splitquery(self.path)
+            action = query[0]
+
+            if query[1]:  # 接收get参数
+                queryParams = {}
+                for qp in query[1].split('&'):
+                    kv = qp.split('=')
+                    queryParams[kv[0]] = urllib.unquote(kv[1]).decode("utf-8", 'ignore')
+                    content += kv[0] + ':' + queryParams[kv[0]] + "\r\n"
+
+            # 指定返回编码
+            enc = "UTF-8"
+            content = content.encode(enc)
+            f = io.BytesIO()
+            f.write(content)
+            f.seek(0)
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=%s" % enc)
+            self.send_header("Content-Length", str(len(content)))
+            self.end_headers()
+            shutil.copyfileobj(f, self.wfile)
+
+server = BaseHTTPServer.HTTPServer(('', 8000), MyRequestHandler)
+print 'started httpserver...'
+server.serve_forever()
+
