a951d0eac96e9fe019bb4a129218275af9510545
冯建涛 <gastonfeng@qq.com>
2017-04-10 16:10:59 +0800

    .

--- str_misc.py
+++ str_misc.py
@@ -1,4 +1,29 @@
+#coding=utf-8
 
+def lst2unicode(lst):
+    st=u''
+    if isinstance(lst,list):
+        st+='('
+        for l in lst:
+            st+="u'%s'"%l
+            st+=','
+        st+=')'
+    return st
+
+una=u'unicode汉字'
+
+lst=[]
+
+lst.append(una)
+lst=lst2unicode(lst)
+
+print (lst)
+for i in lst:
+    print(i)
+
+bstr=str(una)
+
+print bstr
 
 
 tb_yhq_murl='aaa&bbb'
